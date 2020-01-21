36a9a1b5932ea7014fdcc22ece6749f263e308a5
omonimus1 <davidepollicino2015@gmail.com>
2020-01-21 12:22:34 +0000

    Completed FactBook

---
+++ Lab2/factBook/js/PageEvent.js
@@ -0,0 +1,17 @@
+function show_pop()
+{
+    document.getElementById('population').style.display = "block";          
+}
+
+
+function showClimate()
+{
+    document.getElementById("climate").style.display="block";
+}
+
+
+function showArea()
+{
+    document.getElementById("area").style.display="block";
+}
+
