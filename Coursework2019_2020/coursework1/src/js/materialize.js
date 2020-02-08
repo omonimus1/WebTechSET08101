1036b5e98db68d21f5112ea62a6c6dc417563a71
omonimus1 <davidepollicino2015@gmail.com>
2020-02-08 13:31:41 +0000

    Added footer -Select option for story types and  cards and links in sideBar

--- Coursework2019_2020/coursework1/src/js/materialize.js
+++ Coursework2019_2020/coursework1/src/js/materialize.js
@@ -13,6 +13,11 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+
+$(document).ready(function() {
+  $('select').material_select();
+});
+
 /*! cash-dom 1.3.5, https://github.com/kenwheeler/cash @license MIT */
 (function (factory) {
   window.cash = factory();
@@ -465,6 +470,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       }).replace(whiteSpace, "");
     };
   }();
+
 
   var getPrefixedProp = function () {
     var cache = {},
