b5c9666ba761855ae11f5e453400ac79feb3992e
omonimus <davidepollicino2015@gmail.com>
2020-02-14 12:34:03 +0000

    Added erase function ContactMe form

---
+++ Coursework2019_2020/coursework1/src/js/contactMe.js
@@ -0,0 +1,7 @@
+function erase()
+{
+	document.getElementById("full_name").value = "";
+	document.getElementById("message").value = "";
+	document.getElementById("email").value = "";
+	document.getElementById("object").value = "";
+}
